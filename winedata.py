725a15d1558650973cf8a24bf6ccddfa7499e07f
vladimirfilippov <vladdimirfilippov@gmail.com>
2024-10-27 11:17:11 +0300

    Продолжаю работать с wine.csv

--- winedata.py
+++ winedata.py
@@ -2,4 +2,19 @@ import pandas as pd
 
 wines = pd.read_csv('data/wine.csv', index_col=0)
 
-print(wines)
+columns = ['country','province','region_1', 'region_2']
+
+indeces = [0,1,10,100]
+
+top_oceania_wines = wines.loc[(wines.points>=95) & (wines.country.isin(['Australia','New Zealand']))]
+
+mean_point = wines.points.mean()
+
+def remean_points(row):
+    row.points = row.points - mean_point
+    return row
+
+x = wines.apply(remean_points, axis = 'columns')
+
+print(x.points) 
+
